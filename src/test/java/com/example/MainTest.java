a3a74d59b1b6054d362e4ddc78d25ec1bb42e31d
Pavan Tej Medapuram <pmedapuram@salesforce.com>
2018-10-10 15:34:18 -0700

    add a few tests

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void exampleTest1() {
+        System.out.println("This test method should be run");
+        assert 1==1;
+    }
+
+    @Test
+    public void exampleTest2() {
+        System.out.println("This test method should be run");
+        assert 1==1;
+    }
+}
